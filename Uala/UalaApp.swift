8de035f3162bbbd6fd39be2cf9e154d17b301dc1
Mario <cmuoz.altimetrik@ancestry.com>
2025-03-21 12:48:35 -0500

    Initial commit: Set up Xcode project for Cities app

---
+++ Uala/UalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  UalaApp.swift
+//  Uala
+//
+//  Created by Carlos Mario Muñoz on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct UalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UalaTests/UalaTests.swift
@@ -0,0 +1,17 @@
+//
+//  UalaTests.swift
+//  UalaTests
+//
+//  Created by Carlos Mario Muñoz on 21/03/25.
+//
+
+import Testing
+@testable import Uala
+
+struct UalaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
